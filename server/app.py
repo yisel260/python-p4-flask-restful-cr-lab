cbba64c342862a828b10191cd693b0b8b9b50105
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-06-18 14:33:44 -0400

    cmpleted lab

--- server/app.py
+++ server/app.py
@@ -17,10 +17,53 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+
+    def get(self):
+
+        response_dict_list = [p.to_dict() for p in Plant.query.all()]
+
+        response = make_response(
+            response_dict_list,
+            200,
+        )
+
+        return response
+    
+    def post(self):
+        new_plant= Plant(
+            name=request.json['name'],
+            image=request.json['image'],
+            price=request.json['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_dict = new_plant.to_dict()
+
+        response = make_response(
+            response_dict,
+            201,
+        )
+
+        return response
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+
+    def get(self, id):
+
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
+    
         
 
 if __name__ == '__main__':
